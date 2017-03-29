28edb2e6ce82e6317bca018f0173e5ec06c3393e
Naveen Ganessin <naveenganessin@gmail.com>
2017-03-28 20:35:16 -0400

    Added stuff to gradeQuery

--- src/json/StudentQuery.java
+++ src/json/StudentQuery.java
@@ -6,12 +6,17 @@ import database.DFDatabaseCallbackDelegate;
 import database.DFError;
 import database.DFSQL.*;
 import database.WebServer.DFDataUploaderReturnStatus;
+import ui.util.UIStrings;
+import uikit.DFNotificationCenter;
+
+import java.util.ArrayList;
 
 /**
  * Created by gauravsrivastava on 3/13/17.
  */
 public class StudentQuery implements DFDatabaseCallbackDelegate {
-    
+    private JsonObject jsonObject;
+    private boolean getCoursesReturn, getGradeReturn, getCourseGradeReturn;
     /**
      * given a userid returns a list of courses the student is enrolled in
      * returns a list of courseids, title, meetingtime of the course
@@ -22,6 +27,7 @@ public class StudentQuery implements DFDatabaseCallbackDelegate {
         String selectedRows[] = {"courseid"};
         String table1 = "coursestudentmembership";
         String table2 = "students";
+        getCoursesReturn = true;
         try {
             dfsql.select(selectedRows, false, null, null)
                     .from(table1)
@@ -87,10 +93,34 @@ public class StudentQuery implements DFDatabaseCallbackDelegate {
 
     @Override
     public void returnedData(JsonObject jsonObject, DFError error) {
-
+        System.out.println("triggered returnedData");
+        this.jsonObject = null;
+        if(error != null){
+            DFDatabase.print(error.toString());
+            this.jsonObject = null;
+        } else {
+            this.jsonObject = jsonObject;
+        }
+        returnHandler();
     }
 
-    @Override
+    private void returnHandler() {
+        if(getCoursesReturn){
+            ArrayList<String> allCoursesForInstructor = new ArrayList<String>();
+            String courseId = null;
+            try {
+                for (int i = 0; i < jsonObject.get("Data").getAsJsonArray().size(); ++i) {
+                    courseId = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("courseid").getAsString();
+                    allCoursesForInstructor.add(courseId);
+                }
+            }catch (NullPointerException e2){
+                DFNotificationCenter.defaultCenter.post(UIStrings.returned, null);
+            }
+            DFNotificationCenter.defaultCenter.post(UIStrings.returned, allCoursesForInstructor);
+            getCoursesReturn = false;
+        }
+    }
+        @Override
     public void uploadStatus(DFDataUploaderReturnStatus success, DFError error) {
 
     }
