b3af340e30b91ecaff979048f55f27413fa97514
rosenbea <rosenbea@purdue.edu>
2017-04-07 19:35:05 -0400

    Completed Assignment class

--- src/objects/Assignment.java
+++ src/objects/Assignment.java
@@ -7,9 +7,83 @@ import java.io.Serializable;
  * Alex Rosenberg
  */
 public class Assignment implements Serializable {
+    private String title;
+    private int assignmentID;
+    private int courseID;
+    private String openDate;
+    private String dueDate;
+    private String instructions;
+
+    public Assignment(int assignmentID) {
+        this.assignmentID = assignmentID;
+    }
+
+    public Assignment(String title, String instructions, int courseID, String openDate, String dueDate) {
+        this.title = title;
+        this.instructions = instructions;
+        this.courseID = courseID;
+        this.openDate = openDate;
+        this.dueDate = dueDate;
+    }
+
     public Assignment() {
         // empty
     }
 
-    // TODO
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
+
+    public int getAssignmentID() {
+        return this.assignmentID;
+    }
+
+    public void setAssignmentID(int newAssignmentID) {
+        this.assignmentID = newAssignmentID;
+    }
+
+    public int getCourseID() {
+        return this.courseID;
+    }
+
+    public void setCourseID(int newCourseID) {
+        this.courseID = newCourseID;
+    }
+
+    public String getOpenDate() {
+        return this.openDate;
+    }
+
+    public void setOpenDate(String newOpen) {
+        this.openDate = newOpen;
+    }
+
+    public String getDueDate() {
+        return this.dueDate;
+    }
+
+    public void setDueDate(String newDue) {
+        this.dueDate = newDue;
+    }
+
+    public String getInstructions() {
+        return this.instructions;
+    }
+
+    public void setInstructions(String newInstr) {
+        this.instructions = newInstr;
+    }
+
+    public String toString() {
+        return "title: " + this.title
+                + "\nassignmentID: " + this.assignmentID
+                + "\ncourseID: " + this.courseID
+                + "\nopenDate: " + this.openDate
+                + "\ndueDate: " + this.dueDate
+                + "\ninstructions: " + this.instructions;
+    }
 }
