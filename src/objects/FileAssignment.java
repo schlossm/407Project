2af65a1063e109d1d14656b86934f0d02f7f1b88
rosenbea <rosenbea@purdue.edu>
2017-04-10 10:31:25 -0400

    File Assignment, QuizAssignment

--- src/objects/FileAssignment.java
+++ src/objects/FileAssignment.java
@@ -6,10 +6,19 @@ import java.io.Serializable;
  * FileAssignment.java
  * Alex Rosenberg
  */
-public class FileAssignment implements Serializable {
+public class FileAssignment extends Assignment implements Serializable {
+    private String filename;
+
+    public FileAssignment(String filename) {
+        this.filename = filename;
+    }
+
     public FileAssignment() {
         // empty
     }
 
-    // TODO
+    public String toString() {
+        return super.toString()
+                + "\nfilename: " + this.filename;
+    }
 }
