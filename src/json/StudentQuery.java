6b4b43b626ca0481290418759347ad1a26469367
Naveen Ganessin <naveenganessin@gmail.com>
2017-04-18 21:11:38 -0400

    StudentqUery Runnable implementation and userquery changes

--- src/json/StudentQuery.java
+++ src/json/StudentQuery.java
@@ -2,40 +2,58 @@ package json;
 
 import com.google.gson.JsonObject;
 import database.DFDatabase;
-import database.DFDatabaseCallbackDelegate;
-import database.DFError;
 import database.DFSQL.*;
-import database.WebServer.DFDataUploaderReturnStatus;
-import objects.Course;
+import json.util.JSONQueryError;
 import objects.Grade;
-import ui.util.UIStrings;
-import uikit.DFNotificationCenter;
-
 import java.util.ArrayList;
 
 /**
  * Created by gauravsrivastava on 3/13/17.
  */
-public class StudentQuery implements DFDatabaseCallbackDelegate {
+public class StudentQuery {
     private JsonObject jsonObject;
     private boolean getCoursesReturn, getGradeReturn, getCourseGradeReturn;
     /**
      * given a userid returns a list of courses the student is enrolled in
      * returns a list of courseids, title, meetingtime of the course
+     * returns a list of courseids, title, meetingtime of the course
      * @param userid
      */
-    public void getCourses(String userid) {
+    public void getCourses(String userid, QueryCallbackRunnable runnable) {
         DFSQL dfsql = new DFSQL();
         String selectedRows[] = {"courseid"};
         String table1 = "coursestudentmembership";
         String table2 = "students";
         getCoursesReturn = true;
         try {
-            dfsql.select(selectedRows, false, null, null)
-                    .from(table1)
-                    .join(DFSQLJoin.left, table2, table1 + ".studentid", table2 + ".id")
-                    .where(DFSQLEquivalence.equals, table2 + ".userid", "" + userid);
-            DFDatabase.defaultDatabase.execute(dfsql, this);
+            dfsql.select(selectedRows, false, null, null).from(table1).join(DFSQLJoin.left, table2, table1 + ".studentid", table2 + ".id").where(DFSQLEquivalence.equals, table2 + ".userid", "" + userid);
+            DFDatabase.defaultDatabase.execute(dfsql, (response, error) -> {
+                System.out.println(response);
+                System.out.println(error);
+                if (error != null) {
+                    //Process the error and return appropriate new error to UI.
+                    JSONQueryError error1 = new JSONQueryError(0, "Some Error", null/*User info if needed*/);
+                    runnable.run(null, error1);
+                    return;
+                }
+                JsonObject jsonObject;
+                if (response instanceof JsonObject) {
+                    jsonObject = (JsonObject) response;
+                } else {
+                    return;
+                }
+                ArrayList<String> allCoursesForInstructor = new ArrayList<String>();
+                String courseId = null;
+                try {
+                    for (int i = 0; i < jsonObject.get("Data").getAsJsonArray().size(); ++i) {
+                        courseId = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("courseid").getAsString();
+                        allCoursesForInstructor.add(courseId);
+                    }
+                }catch (NullPointerException e2){
+                    runnable.run(null, null);
+                }
+                runnable.run(allCoursesForInstructor, null);
+            });
         } catch (DFSQLError dfsqlError) {
             dfsqlError.printStackTrace();
         }
@@ -47,7 +65,7 @@ public class StudentQuery implements DFDatabaseCallbackDelegate {
      * @param assignmentid
      * @param userid
      */
-    public void getGrade(int assignmentid, String userid) {
+    public void getGrade(int assignmentid, String userid, QueryCallbackRunnable runnable) {
         DFSQL dfsql = new DFSQL();
         String selectedRows[] = {"grade", "assignmentid", "userid"}; //username
         String table1 = "grades";
@@ -59,7 +77,36 @@ public class StudentQuery implements DFDatabaseCallbackDelegate {
                     .from(table1)
                     .join(DFSQLJoin.left, table2, "grade.studentid", "students.id")
                     .where(DFSQLConjunction.and, DFSQLEquivalence.equals, attributes, values);
-            DFDatabase.defaultDatabase.execute(dfsql, this);
+            DFDatabase.defaultDatabase.execute(dfsql, (response, error) -> {
+                System.out.println(response);
+                System.out.println(error);
+                if (error != null) {
+                    //Process the error and return appropriate new error to UI.
+                    JSONQueryError error1 = new JSONQueryError(0, "Some Error", null/*User info if needed*/);
+                    runnable.run(null, error1);
+                    return;
+                }
+                JsonObject jsonObject;
+                if (response instanceof JsonObject) {
+                    jsonObject = (JsonObject) response;
+                } else {
+                    return;
+                }
+                int points = 0;
+                int assignmentId = 0;
+                String userId = "";
+                try {
+                    points = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("grade").getAsInt();
+                    assignmentId = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("assignmentid").getAsInt();
+                    userId = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("userid").getAsString();
+                }catch (NullPointerException e2){
+                    runnable.run(null, null);
+                }
+                Grade grade = new Grade(userId, assignmentId, String.valueOf(points));
+            /* Wait for Alex to implement the rest of the fields */
+                runnable.run(grade, null);
+                System.out.println("getUser posting user to returned");
+            });
         } catch (DFSQLError dfsqlError) {
             dfsqlError.printStackTrace();
         }
@@ -71,7 +118,7 @@ public class StudentQuery implements DFDatabaseCallbackDelegate {
      * @param userid
      * @param courseid
      */
-    public void getCourseGrade(String userid, int courseid) {
+    public void getCourseGrade(String userid, int courseid, QueryCallbackRunnable runnable) {
         DFSQL dfsql = new DFSQL();
         String selectedRows[] = {"Avg(Grade)", "Sum(Grade)"};
         String table1 = "grades";
@@ -84,77 +131,38 @@ public class StudentQuery implements DFDatabaseCallbackDelegate {
                     .join(DFSQLJoin.left, table2, "grades.assignmentid", "assignment.assignmentid")
                     .join(DFSQLJoin.left, table3, "grades.studentid", "students.id")
                     .where(DFSQLConjunction.and, DFSQLEquivalence.equals, attributes, values);
-            DFDatabase.defaultDatabase.execute(dfsql, this);
+            DFDatabase.defaultDatabase.execute(dfsql, (response, error) -> {
+                System.out.println(response);
+                System.out.println(error);
+                if (error != null) {
+                    //Process the error and return appropriate new error to UI.
+                    JSONQueryError error1 = new JSONQueryError(0, "Some Error", null/*User info if needed*/);
+                    runnable.run(null, error1);
+                    return;
+                }
+                JsonObject jsonObject;
+                if (response instanceof JsonObject) {
+                    jsonObject = (JsonObject) response;
+                } else {
+                    return;
+                }
+                int points = 0;
+                int assignmentId = 0;
+                String userId = "";
+                try {
+                    points = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("grade").getAsInt();
+                    assignmentId = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("assignmentid").getAsInt();
+                    userId = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("userid").getAsString();
+                }catch (NullPointerException e2){
+                    runnable.run(null, null);
+                }
+                Grade grade = new Grade(userId, assignmentId, String.valueOf(points));
+            /* Wait for Alex to implement the rest of the fields */
+                runnable.run(grade, null);
+            });
         } catch (DFSQLError dfsqlError) {
             dfsqlError.printStackTrace();
         }
-    }
-
-
-    @Override
-    public void returnedData(JsonObject jsonObject, DFError error) {
-        System.out.println("triggered returnedData");
-        this.jsonObject = null;
-        if(error != null){
-            DFDatabase.print(error.toString());
-            this.jsonObject = null;
-        } else {
-            this.jsonObject = jsonObject;
-        }
-        returnHandler();
-    }
-
-    private void returnHandler() {
-        if(getCoursesReturn){
-            ArrayList<String> allCoursesForInstructor = new ArrayList<String>();
-            String courseId = null;
-            try {
-                for (int i = 0; i < jsonObject.get("Data").getAsJsonArray().size(); ++i) {
-                    courseId = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("courseid").getAsString();
-                    allCoursesForInstructor.add(courseId);
-                }
-            }catch (NullPointerException e2){
-                DFNotificationCenter.defaultCenter.post(UIStrings.returned, null);
-            }
-            DFNotificationCenter.defaultCenter.post(UIStrings.returned, allCoursesForInstructor);
-            getCoursesReturn = false;
-        } else if(getGradeReturn){
-            int points = 0;
-            int assignmentId = 0;
-            String userId = "";
-            try {
-                points = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("grade").getAsInt();
-                assignmentId = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("assignmentid").getAsInt();
-                userId = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("userid").getAsString();
-            }catch (NullPointerException e2){
-                DFNotificationCenter.defaultCenter.post(UIStrings.returned, null);
-            }
-            Grade grade = new Grade(userId, assignmentId, String.valueOf(points));
-            /* Wait for Alex to implement the rest of the fields */
-            DFNotificationCenter.defaultCenter.post(UIStrings.returned, grade);
-            System.out.println("getUser posting user to returned");
-            getGradeReturn = false;
-        } else if(getCourseGradeReturn){
-            int points = 0;
-            int assignmentId = 0;
-            String userId = "";
-            try {
-                points = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("grade").getAsInt();
-                assignmentId = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("assignmentid").getAsInt();
-                userId = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("userid").getAsString();
-            }catch (NullPointerException e2){
-                DFNotificationCenter.defaultCenter.post(UIStrings.returned, null);
-            }
-            Grade grade = new Grade(userId, assignmentId, String.valueOf(points));
-            /* Wait for Alex to implement the rest of the fields */
-            DFNotificationCenter.defaultCenter.post(UIStrings.returned, grade);
-            System.out.println("getUser posting user to returned");
-            getGradeReturn = false;
-        }
-    }
-        @Override
-    public void uploadStatus(DFDataUploaderReturnStatus success, DFError error) {
-
     }
 
 }
--- src/json/UserQuery.java
+++ src/json/UserQuery.java
@@ -18,7 +18,7 @@ import static database.DFDatabase.debugLog;
  * Created by Naveen Ganessin
  */
 
-public class UserQuery implements DFDatabaseCallbackDelegate {
+public class UserQuery{
     private boolean getUserReturn, getUserExistsReturn;
     private DFDataUploaderReturnStatus uploadSuccess;
     private JsonObject jsonObject;
@@ -431,78 +431,8 @@ public class UserQuery implements DFDatabaseCallbackDelegate {
         }
     }
 
-    @Override
-    public void returnedData(JsonObject jsonObject, DFError error) {
-        System.out.println("triggered returnedData");
-        this.jsonObject = null;
-        if(error != null){
-            DFDatabase.print(error.toString());
-            this.jsonObject = null;
-        } else {
-            this.jsonObject = jsonObject;
-        }
-        returnHandler();
-    }
 
 
-    private void returnHandler(){
-        System.out.println("triggers return handler");
-        if(getUserReturn){
-            String usernameReceived = null, userEmail = null, userBirthday = null, userFirstName = null, userLastName = null;
-            int userTypeInt = 0;
-            userType userType = null;
-            try {
-                usernameReceived = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("userID").getAsString();
-                userEmail = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("email").getAsString();
-                userBirthday = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("birthday").getAsString();
-                userFirstName = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("firstName").getAsString();
-                userLastName = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("lastName").getAsString();
-                userTypeInt = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("userType").getAsInt();
-                userType = intToUserTypeConverter(userTypeInt);
-
-            }catch (NullPointerException e2){
-                DFNotificationCenter.defaultCenter.post(UIStrings.returned, null);
-            }
-            User user = new User();
-            user.setUserID(usernameReceived);
-            user.setEmail(userEmail);
-            user.setFirstName(userFirstName);
-            user.setLastName(userLastName);
-            user.setBirthday(userBirthday);
-            user.setUserType(userType);
-
-            DFNotificationCenter.defaultCenter.post(UIStrings.returned, user);
-            System.out.println("getUser posting user to returned");
-            getUserReturn = false;
-        } else if (verifyUserLoginReturn) {
-            System.out.println("gets to verifyuserlogin");
-            String databasePassword = "";
-            try {
-                databasePassword = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("password").getAsString();
-                if(databasePassword.equals(bufferString)){DFNotificationCenter.defaultCenter.post(UIStrings.success, Boolean.TRUE);
-                    debugLog("verifylogin returned success");}
-                else {DFNotificationCenter.defaultCenter.post(UIStrings.failure, Boolean.FALSE);
-                    debugLog("verifylogin returned fail cause passwords don't match");}
-            } catch (NullPointerException e2){
-                DFNotificationCenter.defaultCenter.post(UIStrings.failure, Boolean.FALSE);
-                debugLog("verifylogin returned nothing");
-            }
-            verifyUserLoginReturn = false;
-        } else if (getUserExistsReturn) {
-            String usernameReceived = null;
-            try {
-                usernameReceived = jsonObject.get("Data").getAsJsonArray().get(0).getAsJsonObject().get("userID").getAsString();
-            }catch (NullPointerException e2){
-                DFNotificationCenter.defaultCenter.post(UIStrings.exists, false);
-            }
-            if(usernameReceived != null){
-                DFNotificationCenter.defaultCenter.post(UIStrings.exists, true);
-            }
-        }
-
-        getUserExistsReturn = false;
-        bufferString = null;
-    }
 
     public void addNewUser(String userUserId, String userPassword,  String userEmail, String userBirthday, String userFirstName, String userLastName, userType userUserType, QueryCallbackRunnable runnable)
     {
@@ -579,23 +509,6 @@ public class UserQuery implements DFDatabaseCallbackDelegate {
         } else{
             return userType.TA;
         }
-    }
-
-    @Override
-    public void uploadStatus(DFDataUploaderReturnStatus success, DFError error) {
-        this.uploadSuccess = null;
-        if(success == DFDataUploaderReturnStatus.success){
-            debugLog("success uploading this");
-        } else if (success == DFDataUploaderReturnStatus.failure) {
-            debugLog("Failure uploading this");
-        }
-        else if(success == DFDataUploaderReturnStatus.error){
-            debugLog("Error uploading this");
-            DFDatabase.print(error.toString());
-        } else {
-            debugLog("I have no clue!");
-        }
-        this.uploadSuccess = success;
     }
 
     public static void main(String[] args){
