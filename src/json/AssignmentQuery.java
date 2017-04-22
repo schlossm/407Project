d16ad6fed861befd946e51b5bef50b652d87397d
srivast6 <gsrivastava93@gmail.com>
2017-04-21 23:14:42 -0400

    shifted assignment related methods to assignment query and implemented getting quizzes from a course

--- src/json/AssignmentQuery.java
+++ src/json/AssignmentQuery.java
@@ -5,10 +5,10 @@ import database.DFDatabase;
 import database.DFDatabaseCallbackDelegate;
 import database.DFDatabaseCallbackRunnable;
 import database.DFError;
-import database.DFSQL.DFSQL;
-import database.DFSQL.DFSQLError;
+import database.DFSQL.*;
 import database.WebServer.DFDataUploaderReturnStatus;
 import json.util.JSONQueryError;
+import objects.Assignment;
 import objects.Question;
 import objects.QuizAssignment;
 
@@ -145,11 +145,6 @@ public class AssignmentQuery  {
         }
     }
 
-    public void removeQuiz(int assignment) {
-
-
-    }
-
     public void getQuiz(int assignmentId, QueryCallbackRunnable runnable) {
         DFSQL dfsql = new DFSQL();
         String[] selectRows = {"assignmentId", "point", "question", "choices", "correctAnswer"};
@@ -212,31 +207,198 @@ public class AssignmentQuery  {
         return choiceList;
     }
 
-    public void modifyquiz(int quizid) {
-
-    }
-
-    public void addGradeForQuiz(int assignm, double grade) {
-
-    }
-
-    /**
-     * returns the
-     * @param quizid
-     * @param userid
-     * @param studentQuizAnswer
-     */
-    public void submitQuizByStudent(int quizid, String userid, QuizAssignment studentQuizAnswer) {
-
-    }
-
 
     /**
      * id, name, duedate
      * @param courseid
      */
-    public void getAllQuizes(int courseid) {
+    public void getAllQuizzesInCourse(int courseid) {
+        DFSQL dfsql = new DFSQL();
+        String[] selectedRows = {"assignmentId", "name", "type", "courseid", "deadline"};
+        String[] attrs = {"courseid", "type"};
+        String[] values = {courseid + "", "quiz"};
+        String table = "assignment";
+        try {
+            dfsql.select(selectedRows, false, null, null).from(table)
+                    .where(DFSQLConjunction.and, DFSQLEquivalence.equals, attrs, values);
+            DFDatabase.defaultDatabase.execute(dfsql, (response, error) -> {
 
+            });
+        } catch (DFSQLError e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    /**
+     * Returns all info of an assignment given the assignment id
+     * @param assignmentid
+     */
+    public void getAssignmentInfo(int assignmentid, QueryCallbackRunnable runnable) {
+        DFSQL dfsql = new DFSQL();
+        String[] selectedRows = {"assignmentid, name, courseid", "maxpoints", "type", "deadline"};
+        String table = "assignment";
+        try {
+            dfsql.select(selectedRows, false, null, null).from(table).where(DFSQLEquivalence.equals, "assignmentid", "" + assignmentid);
+            DFDatabase.defaultDatabase.execute(dfsql, (response, error) -> {
+
+            });
+        } catch (DFSQLError e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public void addAssignment(int courseid, String name, String deadline, double maxPoints, String type, QueryCallbackRunnable runnable) {
+        DFSQL dfsql = new DFSQL();
+        String[] rows = {"name", "courseid", "maxpoints", "type", "deadline"};
+        String[] values = {name, courseid + "","" + maxPoints, type, deadline};
+        String table = "assignment";
+        try {
+            dfsql.insert(table, values, rows);
+            DFDatabase.defaultDatabase.execute(dfsql, (response, error) ->
+            {
+                if (error != null)
+                {
+                    JSONQueryError error1;
+                    if (error.code == 2)
+                    {
+                        error1 = new JSONQueryError(1, "Duplicate ID", null);
+                    }
+                    else if (error.code == 3)
+                    {
+                        error1 = new JSONQueryError(2, "Duplicate Unique Entry", null);
+                    }
+                    else
+                    {
+                        error1 = new JSONQueryError(0, "Internal Error", null);
+                    }
+                    runnable.run(null, error1);
+                    return;
+                }
+
+                if (response instanceof DFDataUploaderReturnStatus)
+                {
+                    DFDataUploaderReturnStatus returnStatus = (DFDataUploaderReturnStatus)response;
+                    if (returnStatus == DFDataUploaderReturnStatus.success)
+                    {
+                        runnable.run(true, null);
+                    }
+                    else
+                    {
+                        runnable.run(false, null);
+                    }
+                }
+                else
+                {
+                    runnable.run(null, new JSONQueryError(0, "Internal Error", null));
+                }
+            });
+        } catch (DFSQLError e1)
+        {
+            e1.printStackTrace();
+        }
+    }
+
+    /**
+     * remove an assignment from a course
+     * @param assignmentid remove assignment given assignment
+     */
+    public void removeAssignment(int assignmentid, QueryCallbackRunnable runnable) {
+        DFSQL dfsql = new DFSQL();
+        String table = "assignment";
+        try {
+            dfsql.delete(table, new Where(DFSQLConjunction.none, DFSQLEquivalence.equals, new DFSQLClause("assignmentid", "" + assignmentid)));
+            DFDatabase.defaultDatabase.execute(dfsql, (response, error) ->
+            {
+                if (error != null)
+                {
+                    JSONQueryError error1;
+                    if (error.code == 2)
+                    {
+                        error1 = new JSONQueryError(1, "Duplicate ID", null);
+                    }
+                    else if (error.code == 3)
+                    {
+                        error1 = new JSONQueryError(2, "Duplicate Unique Entry", null);
+                    }
+                    else
+                    {
+                        error1 = new JSONQueryError(0, "Internal Error", null);
+                    }
+                    runnable.run(null, error1);
+                    return;
+                }
+
+                if (response instanceof DFDataUploaderReturnStatus)
+                {
+                    DFDataUploaderReturnStatus returnStatus = (DFDataUploaderReturnStatus)response;
+                    if (returnStatus == DFDataUploaderReturnStatus.success)
+                    {
+                        runnable.run(true, null);
+                    }
+                    else
+                    {
+                        runnable.run(false, null);
+                    }
+                }
+                else
+                {
+                    runnable.run(null, new JSONQueryError(0, "Internal Error", null));
+                }
+            });
+        } catch (DFSQLError e1)
+        {
+            e1.printStackTrace();
+        }
+    }
+
+    /**
+     * Get all the assignments of a course
+     * Return a list of all ids of an assignment
+     * @param courseid courseid such as 1111 of the course
+     */
+    public void getAllAssignmentsInCourse(int courseid, QueryCallbackRunnable runnable) {
+        DFSQL dfsql = new DFSQL();
+        String selectedRows[] = {"assignmentid", "name"};
+        String table = "assignment";
+        try {
+            dfsql.select(selectedRows, false, null, null).from(table).where(DFSQLEquivalence.equals, "courseid", "" + courseid);
+            DFDatabase.defaultDatabase.execute(dfsql, (response, error) -> {
+                System.out.println(response);
+                System.out.println(error);
+                JSONQueryError error1 = new JSONQueryError(0, "Some Error", null/*User info if needed*/);
+                if (error != null) {
+                    //Process the error and return appropriate new error to UI.
+                    runnable.run(null, error1);
+                    return;
+                }
+                JsonObject jsonObject;
+                if (response instanceof JsonObject) {
+                    jsonObject = (JsonObject) response;
+                } else {
+                    return;
+                }
+                ArrayList<Assignment> allAssignmentsInCourse = new ArrayList<Assignment>();
+                String name = null;
+                int assignmentId = 0;
+                Assignment assignment;
+                try {
+                    for (int i = 0; i < jsonObject.get("Data").getAsJsonArray().size(); ++i) {
+                        assignmentId = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("assignmentid").getAsInt();
+                        name = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("name").getAsString();
+
+
+                        assignment = new Assignment();
+
+                        assignment.setAssignmentID(assignmentId);
+                        assignment.setTitle(name);
+                        allAssignmentsInCourse.add(assignment);
+                    }
+                }catch (NullPointerException e2){runnable.run(null, error1);}
+                runnable.run(allAssignmentsInCourse, null);
+            });
+        } catch (DFSQLError e1) {
+            e1.printStackTrace();
+        }
     }
 
 
