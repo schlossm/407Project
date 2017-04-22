69290b791793560517b906acf2c27ed708c0bde0
srivast6 <gsrivastava93@gmail.com>
2017-04-21 22:52:04 -0400

    getting announcement for a student

--- src/json/AnnouncementQuery.java
+++ src/json/AnnouncementQuery.java
@@ -131,14 +131,75 @@ public class AnnouncementQuery
 
 	public void getAllAnnouncementForStudent(String userid, QueryCallbackRunnable runnable) {
 		DFSQL dfsql = new DFSQL();
-		String[] selectedRows = {"id", "title", "content", "timestamp", "authoruserid", "courseid"};
-		String table1 = "Annoucement";
+		DFSQL dfsql1 = new DFSQL();
+		String[] selectedRows = {"Announcement.id", "title", "content", "timestamp", "authoruserid", "Announcement.courseid"};
+		String table1 = "Announcement";
 		String table2 = "coursestudentmembership";
 		String table3 = "users";
 		Join[] joins = new Join[] {
 				new Join(table2, table1 + ".courseid", table2 + ".courseid"),
 				new Join(table3, table3 + ".userid", table2 + ".userid")};
+		try {
+			dfsql.select(selectedRows, false, null, null)
+					.from(table1)
+					.join(DFSQLJoin.left, joins)
+					.where(DFSQLEquivalence.equals, "userid", "" + userid);
+			dfsql1.select(selectedRows, false, null, null)
+					.from(table1)
+					.where(DFSQLEquivalence.equals, "courseid", "-1");
+			dfsql.append(dfsql1);
+			DFDatabase.defaultDatabase.execute(dfsql, (response, error) -> {
+				if (error != null)
+				{
+					JSONQueryError error1;
+					if (error.code == 1)
+					{
+						error1 = new JSONQueryError(3, "No Data", null);
+					}
+					else {
+						error1 = new JSONQueryError(0, "Internal Error", null);
+					}
+					runnable.run(null, error1);
+					return;
+				}
+				if (response instanceof JsonObject)
+				{
+					JsonObject jsonObject = (JsonObject) response;
 
+					ArrayList<Message> allAnnouncementsInCourse = new ArrayList<Message>();
+					int courseId;
+					String authorUserId, title, content, timestamp;
+					Message announcement;
+
+					try
+					{
+						for (int i = 0; i < jsonObject.get("Data").getAsJsonArray().size(); ++i)
+						{
+							courseId = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("courseid").getAsInt();
+							authorUserId = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("authoruserid").getAsString();
+							title = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("title").getAsString();
+							content = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("content").getAsString();
+							timestamp = jsonObject.get("Data").getAsJsonArray().get(i).getAsJsonObject().get("timestamp").getAsString();
+							announcement = new Message(title, content, timestamp, authorUserId, courseId);
+							allAnnouncementsInCourse.add(announcement);
+						}
+					}
+					catch (NullPointerException e2)
+					{
+						JSONQueryError error1 = new JSONQueryError(0, "Internal Error", null);
+						runnable.run(null, error1);
+					}
+					runnable.run(allAnnouncementsInCourse, null);
+				}
+				else
+				{
+					JSONQueryError error1 = new JSONQueryError(0, "Internal Error", null);
+					runnable.run(null, error1);
+				}
+			});
+		} catch (DFSQLError dfsqlError) {
+			dfsqlError.printStackTrace();
+		}
 	}
 
 	private void returnHandler()
